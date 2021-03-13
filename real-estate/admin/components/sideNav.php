ec5347d723e84a60ba4453f43028a76b29a6ba8a
coder-rohit <rohit.panchal.737@gmail.com>
2021-03-14 01:32:32 +0530

    Add files via upload

--- real-estate/admin/components/sideNav.php
+++ real-estate/admin/components/sideNav.php
@@ -2,11 +2,13 @@
 
     $sideNav = "<div id='mySidenav' class='sidenav'>
     <a href=''>Admin Tools</a>
-    <a href='#'></a>
+    <a href='../index.php' style='font-size:16px; margin-top: -19px'>View Site</a>
+    <a></a>
     <a href='./admin.php'>Add Property</a>
     <a href='./editProps.php'>Edit Properties</a>
     <a href='./requirementsBuyRent.php'>Prop. Requirements</a>
     <a href='./propertySellRent.php'>Sell Rent Props</a>
+    <a href='./logout.php'>Logout</a>
   </div>";
 
 ?>
